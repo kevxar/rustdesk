a6b5328fe082de955434b761ec0b0adf6eeb05af
Kevin Araya Reygada <kevinarayareygada@gmail.com>
2026-09-26 15:47:54 -0300

    Preserve upstream attribution in corporate packages

--- res/msi/preprocess.py
+++ res/msi/preprocess.py
@@ -499,7 +499,8 @@ def update_license_file(app_name):
         license_content = f.read()
     license_content = license_content.replace("website rustdesk.com and other ", "")
     license_content = license_content.replace("RustDesk", app_name)
-    license_content = re.sub(r"Purslane(?: Tech Pte\.)? Ltd", app_name, license_content, flags=re.IGNORECASE)
+    # Product branding may change, but the upstream copyright holder must remain
+    # identified in the license distributed with a derivative build.
     with open(license_file, "w", encoding="utf-8") as f:
         f.write(license_content)
 
